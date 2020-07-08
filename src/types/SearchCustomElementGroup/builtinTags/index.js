e90d3ebca8b798ac180d2e14f8253b1f1a2873b1
Qixuan Hong <frederickhong.5@gmail.com>
2020-07-08 01:55:30 -0700

    add builtin tags initial files

---
+++ src/types/SearchCustomElementGroup/builtinTags/index.js
@@ -0,0 +1,15 @@
+import searchIntro from './searchIntro';
+import searchBar from './searchBar';
+import searchItemsList from './searchItemsList';
+import searchListItem from './searchListItem';
+import searchGridItem from './searchGridItem';
+import searchPagination from './searchPagination';
+
+export default {
+  'search-intro': searchIntro,
+  'search-bar': searchBar,
+  'search-items-list': searchItemsList,
+  'search-list-item': searchListItem,
+  'search-grid-item': searchGridItem,
+  'search-pagination': searchPagination,
+};
